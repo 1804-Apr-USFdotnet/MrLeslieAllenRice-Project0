25d779179e39f73263ded43b8d4272d924c646e6
System Administrator <root@USF-Gold-Wifi-226-117-188.laptops.usf.edu>
2018-04-23 11:35:44 -0400

    CodeChallenge1

---
+++ CodeChallenge1/CodeChallenge1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Palindrome;
+
+namespace CodeChallengeOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rc = "racecar";
+
+            Class1 pd = new Class1(rc);
+            bool result = pd.PalindromeStringCheck(rc);
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ CodeChallenge1/Palindrome/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Palindrome
+{
+    public class Class1
+    {
+        string sStringToCheck;
+        bool result;
+
+        public string StringToCheck { get; set; }
+
+        public Class1()
+        {
+            sStringToCheck = null;
+        }
+
+        public Class1(string _sStringToCheck)
+        {
+            sStringToCheck = _sStringToCheck;
+        }
+
+        public bool PalindromeStringCheck(string _sStringToCheck)
+        {
+            string saComparisonString;
+            saComparisonString = _sStringToCheck;
+
+            Console.WriteLine("Comparison String: " + saComparisonString);
+
+            for (int i = _sStringToCheck.Length-1; i >= 0; i--)
+            {
+                if(saComparisonString[i] == sStringToCheck[i])
+                {
+                    Console.WriteLine(saComparisonString[i]);
+                    result = true;
+                }
+                else if(saComparisonString[i] != sStringToCheck[i])
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
