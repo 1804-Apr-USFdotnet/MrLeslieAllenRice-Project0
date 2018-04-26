26e3688d3e37edd087667a00bb66175e9bbd66ad
System Administrator <root@Leslies-MacBook-Pro.local>
2018-04-25 22:29:38 -0400

    RestaurantReviewPresentationLayer

---
+++ RestaurantReviewPresentationLayer/RestaurantAndRestaurantReviewDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using RestaurantReviewBuisnessLayer;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantReviewPresentationLayer
+{
+    public static class RestaurantAndRestaurantReviewDisplay
+    {
+        public static void DisplayRestaurantDetails(Restaurant r)
+        {
+            Console.WriteLine(r.sName);
+            Console.WriteLine("Address: " + r.sAddress);
+            Console.WriteLine("Average rating: " + r.dAvgRating);
+        }
+
+        public static void DisplayReviewsForRestaurant(List<Review> rr)
+        {
+            foreach(Review r in rr)
+            {
+                Console.WriteLine(r.sRestaurantName);
+                Console.WriteLine("Review: " + r.sReviewSummary);
+                Console.WriteLine("Rating: " + r.dRating);
+            }
+        }
+    }
+}
