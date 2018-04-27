c0ba1a1cb062e052371ce4eb3792b3c4462691e7
System Administrator <root@Leslies-MacBook-Pro.local>
2018-04-27 17:43:08 -0400

    RestaurantReviewBuisnessLayer

--- RestaurantReviewBuisnessLayer/SerializerDeserializerHelper.cs
+++ RestaurantReviewBuisnessLayer/SerializerDeserializerHelper.cs
@@ -4,7 +4,9 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using System.Xml.Serialization;
+using System.Runtime.Serialization;
 
 namespace RestaurantReviewBuisnessLayer
 {
@@ -12,38 +14,115 @@ namespace RestaurantReviewBuisnessLayer
     {
         public static void SerializeRestaurants(List<Restaurant> r)
         {
-            XmlSerializer serializeRestaurants = new XmlSerializer(typeof(List<Restaurant>));
-            using (TextWriter writer = new StreamWriter(@"C:\Users\leslierice\source\repos\Restaurants.txt"))
+            try
             {
-                serializeRestaurants.Serialize(writer, r);
+                XmlSerializer serializeRestaurants = new XmlSerializer(typeof(List<Restaurant>));
+                using (TextWriter writer = new StreamWriter(@"C:\Users\leslierice\source\repos\Restaurants.txt"))
+                {
+                    serializeRestaurants.Serialize(writer, r);
+                }
             }
+            catch (SerializationException se)
+            {
+                Console.WriteLine("Exception handled: " + se);
+            }
+            catch(SystemException syse)
+            {
+                Console.WriteLine("Exception handled: " + syse);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception handled: " + e);
+            }
+            
         }
 
         public static void SerializeRestaurantReviews(List<Review> r)
         {
-            XmlSerializer serializeRestaurantReviews = new XmlSerializer(typeof(List<Review>));
-            using (TextWriter writer = new StreamWriter(@"C:\Users\leslierice\source\repos\RestaurantReviews.txt"))
+            try
             {
-                serializeRestaurantReviews.Serialize(writer, r);
+                XmlSerializer serializeRestaurantReviews = new XmlSerializer(typeof(List<Review>));
+                using (TextWriter writer = new StreamWriter(@"C:\Users\leslierice\source\repos\RestaurantReviews.txt"))
+                {
+                    serializeRestaurantReviews.Serialize(writer, r);
+                }
+            }
+            catch (SerializationException se)
+            {
+                Console.WriteLine("Exception handled: " + se);
+            }
+            catch (SystemException syse)
+            {
+                Console.WriteLine("Exception handled: " + syse);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception handled: " + e);
             }
         }
 
         public static void DeserializeRestaurants(List<Restaurant> r)
         {
-            XmlSerializer deserializeRestaurants = new XmlSerializer(typeof(List<Restaurant>));
             TextReader reader = new StreamReader(@"C:\Users\leslierice\source\repos\Restaurants.txt");
-            object restaurants = deserializeRestaurants.Deserialize(reader);
-            List<Restaurant> XmlData = (List<Restaurant>)restaurants;
-            reader.Close();
+            
+            try
+            {
+                XmlSerializer deserializeRestaurants = new XmlSerializer(typeof(List<Restaurant>));
+                object restaurants = deserializeRestaurants.Deserialize(reader);
+                List<Restaurant> XmlData = (List<Restaurant>)restaurants;
+            }
+            catch(InvalidOperationException ioe)
+            {
+                Console.WriteLine("Exception: " + ioe);
+            }
+            catch(XmlException xe)
+            {
+                Console.WriteLine("Exception: " + xe);
+            }
+            catch(SystemException se)
+            {
+                Console.WriteLine("Exception: " + se);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception handled: " + e);
+            }
+            finally
+            {
+                reader.Close();
+            }
         }
 
         public static void DeserializeRestaurantReviews(List<Review> r)
         {
-            XmlSerializer deserializeRestaurantReviews = new XmlSerializer(typeof(List<Review>));
             TextReader reader = new StreamReader(@"C:\Users\leslierice\source\repos\RestaurantReviews.txt");
-            object reviews = deserializeRestaurantReviews.Deserialize(reader);
-            List<Restaurant> XmlData = (List<Restaurant>)reviews;
-            reader.Close();
+
+            try
+            {
+                XmlSerializer deserializeRestaurantReviews = new XmlSerializer(typeof(List<Review>));
+                object reviews = deserializeRestaurantReviews.Deserialize(reader);
+                List<Restaurant> XmlData = (List<Restaurant>)reviews;
+            }
+            catch(InvalidOperationException ioe)
+            {
+                Console.WriteLine("Exception: " + ioe);
+            }
+            catch (XmlException xe)
+            {
+                Console.WriteLine("Exception: " + xe);
+            }
+            catch (SystemException se)
+            {
+                Console.WriteLine("Exception: " + se);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception handled: " + e);
+            }
+            finally
+            {
+                reader.Close();
+            }
         }
     }
 }
